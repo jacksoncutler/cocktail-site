616a9559cef01f010d24177aa8d1e1475aa42c77
Jackson <jackson.cutler@gmail.com>
2022-07-09 15:23:28 -0700

    Extend tailwind theme
    Add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    extend: {},
+    fontSize: {
+      base: ['1rem', { lineHeight: '1.5rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.5rem', { lineHeight: '2rem' }],
+      '2xl': ['2.25rem', { lineHeight: '2.5rem' }],
+    },
+    fontFamily: {
+      serif: ['PT Serif', 'serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#000000',
+        darkgrey: '#2c2c2c',
+        lightgrey: '#f5f5f5',
+      },
+    },
   },
   plugins: [],
 }
