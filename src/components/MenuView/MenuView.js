82d9811d4e8e62ead69204aa747df53be79e6bc4
Jackson <jackson.cutler@gmail.com>
2022-08-25 17:33:59 -0700

    Implement MenuItem component in MenuView

--- src/components/MenuView/MenuView.js
+++ src/components/MenuView/MenuView.js
@@ -1,4 +1,6 @@
 import { Subheading } from 'components'
+import { MenuItem } from 'components'
+import drinkPic from 'assets/drink.png'
 import { menuViewStyle } from './style'
 
 export const MenuView = () => {
@@ -6,10 +8,22 @@ export const MenuView = () => {
   const placeholders = [...Array(101).keys()]
   return (
     <div className={style.outer}>
-      <Subheading label='Bourbon' />
+      <Subheading label='Whiskey' size='lg' />
       <div className={style.section}>
         {
-          placeholders.map(n => { return <div className='h-thumbnail w-full bg-grey-100'>{n}</div> })
+          placeholders.map(n => { return n%2 === 0 ?
+              <MenuItem 
+                name='Old Fashioned' 
+                desc='Bourbon - Simple Syrup - Angostura Bitters - Orange Bitters - Orange Twist'
+                thumbnail={drinkPic}
+              />
+              :
+              <MenuItem 
+                name='Spicy Avocado Mint Margarita' 
+                desc='Puerto Rican Rum - Jamaican Rum - Allspice Dram - Lime Juice - Orange Juice - Cinnamon Syrup - Vanilla Syrup - Angostura Bitters'
+                thumbnail={drinkPic}
+              />
+          })
         }
       </div>
     </div>
--- src/components/MenuView/style.js
+++ src/components/MenuView/style.js
@@ -1,6 +1,6 @@
 export const menuViewStyle = () => {
     return {
         outer: 'absolute top-header bottom-0 left-sidebar right-0 p-10 overflow-scroll',
-        section: 'pt-6 grid grid-cols-2 gap-x-10 gap-y-6',
+        section: 'pt-6 grid grid-cols-1 lg:grid-cols-2 gap-x-10 gap-y-6',
     }
 }
