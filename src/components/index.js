3765fe1e8f58133c3c76a639af0057c1a9723708
Jackson <jackson.cutler@gmail.com>
2022-08-25 17:33:37 -0700

    Stub out MenuItem component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export * from './App'
 export * from './FilterItem'
 export * from './Header'
+export * from './MenuItem'
 export * from './MenuView'
 export * from './Sidebar'
 export * from './Subheading'
