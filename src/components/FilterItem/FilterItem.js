70a65028cb8553c657a6901322dfd2b1a9c47e2f
Jackson <jackson.cutler@gmail.com>
2022-08-25 17:25:40 -0700

    Indent wrapped text on FilterItem

--- src/components/FilterItem/FilterItem.js
+++ src/components/FilterItem/FilterItem.js
@@ -1,11 +1,11 @@
 import { filterItemStyle } from './style'
 
 export const FilterItem = ({ label }) => {
-    const style = filterItemStyle()
-    return (
-        <label className={style.outer}>
-            <input type='checkbox' />
-            <span className={style.label}>{ label }</span>
-        </label>
-    )
+  const style = filterItemStyle()
+  return (
+    <label className={style.outer}>
+      <input type='checkbox' className={style.checkbox} />
+      <span className={style.label}>{ label }</span>
+    </label>
+  )
 }
