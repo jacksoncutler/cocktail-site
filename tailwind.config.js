ccb1762e58d1680025b9efb2533dd5ad3ee435e9
Jackson <jackson.cutler@gmail.com>
2022-08-25 17:23:41 -0700

    Update tailwind theme
    Increase lg screen breakpoint, add font sizes, add colors, decrease width of sidebar spacing var

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,16 @@ module.exports = {
     screens: {
       sm: '480px',
       md: '768px',
-      lg: '976px',
+      lg: '1200px',
       xl: '1440px',
     },
     fontSize: {
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],
       base: ['1rem', { lineHeight: '1.5rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.5rem' }],
       xl: ['1.5rem', { lineHeight: '2rem' }],
-      '2xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '2xl': ['2rem', { lineHeight: '2.5rem' }],
+      '3xl': ['2.25rem', { lineHeight: '2.5rem' }],
     },
     fontFamily: {
       sans: ['PT Serif', 'serif'],
@@ -21,12 +23,14 @@ module.exports = {
       colors: {
         primary: '#000000',
         'grey-100': '#f5f5f5',
+        'grey-300': '#e6e6e6',
         'grey-400': '#dadada',
+        'grey-800': '#3f3f3f',
         'grey-900': '#2c2c2c',
       },
       spacing: {
         'header': '5rem',
-        'sidebar': '16rem',
+        'sidebar': '14rem',
         'thumbnail': '9.375rem',
       },
     },
