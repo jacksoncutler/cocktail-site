f63a530b7845c639c306d9e31a7d2daff4ac3862
Jackson <jackson.cutler@gmail.com>
2022-08-25 17:32:21 -0700

    Add drop shadow to Header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,6 +1,6 @@
 export const headerStyle = () => {
     return {
-        outer: 'absolute left-0 right-0 bg-primary py-5 pl-5',
-        title: 'text-xl text-white lg:text-2xl'
+        outer: 'absolute left-0 right-0 bg-primary py-5 pl-5 drop-shadow-lg',
+        title: 'text-xl text-white md:text-2xl'
     }
 }
