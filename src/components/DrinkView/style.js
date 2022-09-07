ecb7cb0922737b943f0306fc179aafa078e09464
Jackson <jackson.cutler@gmail.com>
2022-09-07 15:49:05 -0700

    Update padding on ViewPort

--- src/components/DrinkView/style.js
+++ src/components/DrinkView/style.js
@@ -1,6 +1,6 @@
 export const drinkViewStyle = () => {
     return {
-        outer: 'w-full flex flex-row items-start pt-4',
+        outer: 'w-full flex flex-row items-start pt-12',
         img: 'min-w-img basis-1/3 mr-16 shadow-lg',
         inner: 'basis-2/3',
         textSpacing: 'mt-10'
--- src/components/ViewPort/style.js
+++ src/components/ViewPort/style.js
@@ -1,5 +1,5 @@
 export const viewPortStyle = () => {
     return {
-        outer: 'absolute top-header bottom-0 left-sidebar right-0 p-16 overflow-scroll'
+        outer: 'absolute top-header bottom-0 left-sidebar right-0 py-8 px-16 overflow-scroll'
     }
 }
