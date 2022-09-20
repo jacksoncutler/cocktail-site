89691f268a126719f19fb323687ff7809b41825a
Jackson <jackson.cutler@gmail.com>
2022-09-19 17:30:25 -0700

    Hide sidebar on mobile
    Refers #4

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,5 +1,6 @@
 export const sidebarStyle = () => {
     return {
-        outer: 'absolute top-header bottom-0 w-sidebar overflow-scroll px-7 py-8 bg-grey-100 shadow-lg text-grey-900'
+        outer: 'absolute top-header bottom-0 -left-sidebar md:left-0 w-sidebar \
+            overflow-scroll px-7 py-8 bg-grey-100 shadow-lg text-grey-900'
     }
 }
--- src/components/ViewPort/style.js
+++ src/components/ViewPort/style.js
@@ -1,5 +1,6 @@
 export const viewPortStyle = () => {
     return {
-        outer: 'absolute top-header bottom-0 left-sidebar right-0 py-8 px-16 overflow-scroll'
+        outer: 'absolute top-header bottom-0 left-0 md:left-sidebar right-0 \
+            py-8 px-16 overflow-scroll'
     }
 }
