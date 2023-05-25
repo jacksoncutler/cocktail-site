fccea2ce8a70569a232f9c6b328564472b567510
Jackson <jackson.cutler@gmail.com>
2023-05-25 13:55:15 -0700

    Handle adding and removing filters from FilterItem component
    Refers #17

--- src/components/FilterItem/FilterItem.js
+++ src/components/FilterItem/FilterItem.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
+import { useQuery } from 'contexts/QueryContext'
 import { filterItemStyle } from './style'
 
 export const FilterItem = ({ id, label }) => {
+  const [checked, setChecked] = useState(false)
+  const { filters, addFilter, removeFilter } = useQuery()
+
+  useEffect(() => {
+    setChecked(filters.includes(id))
+  }, [])
+
+  const toggleFilter = () => {
+    if (!checked) {
+      addFilter(id)
+    } else {
+      removeFilter(id)
+    }
+    setChecked(!checked)
+  }
+  
   const style = filterItemStyle()
   return (
     <label className={style.outer}>
-      <input type='checkbox' className={style.checkbox} />
+      <input 
+        type='checkbox' 
+        checked={checked} 
+        onChange={toggleFilter}
+        className={style.checkbox}  />
       <span className={style.label}>{ label }</span>
     </label>
   )
