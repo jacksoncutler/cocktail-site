c7c2401bfdb9cf0dbd29686ca19d13a215a963b3
Jackson <jackson.cutler@gmail.com>
2024-06-11 13:32:26 -0700

    Remove default ingredient image

--- src/components/DrinkImage/DrinkImage.js
+++ src/components/DrinkImage/DrinkImage.js
@@ -1,9 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button } from 'components';
 import { getImageURL } from 'util/firebase';
-import ingredientPic from 'assets/ingredients.png';
-import LeftSVG from 'assets/left.svg';
-import RightSVG from 'assets/right.svg';
 import { drinkImageStyle } from './style';
 
 export const DrinkImage = ({
@@ -12,28 +8,17 @@ export const DrinkImage = ({
   className,
 }) => {
   const [drinkImageUrl, setDrinkImageUrl] = useState();
-  const [ingredientsImageUrl, setIngredientsImageUrl] = useState();
-  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     if(drinkImageKey) getImageURL(drinkImageKey).then((url) => setDrinkImageUrl(url));
-    // getImageURL(ingredientsImageKey).then((url) => setIngredientsImageUrl(url));
   }, [drinkImageKey, ingredientsImageKey]);
 
-  // CHANGE INGREDIENT PIC
-  const images = [drinkImageUrl, ingredientPic];
-  const changeImage = () => setIndex(index => (index + 1) % 2);
-
-  const renderImage = () => <img src={images[index]} alt='drink' />;
-  const leftIcon = <img src={LeftSVG} alt='left' />;
-  const rightIcon = <img src={RightSVG} alt='left' />;
+  const renderImage = () => <img src={drinkImageUrl} alt='drink' />;
 
   const style = drinkImageStyle(className);
   return (
     <div className={style.outer}>
       {renderImage()}
-      <Button onClick={changeImage} icon={leftIcon} className={style.left} />
-      <Button onClick={changeImage} icon={rightIcon} className={style.right} />
     </div>
   );
 };
